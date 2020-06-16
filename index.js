23c9067fd7b4fb77c1821af51f01e0e53322d5ff
realyxf <997915421@qq.com>
2020-06-17 00:06:04 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var user={
+    name: "John" ,
+    surname: "Mike" ,
+};
+console.log(user);
+user.name = "Peter";
+console.log(user);
+delete user.name;
+console.log(user);
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+    };
+console.log(fruit.apple + fruit.pear + fruit.peach);
